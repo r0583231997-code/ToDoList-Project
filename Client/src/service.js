66a956578d66eb93b0080aaefea50ab184b99eaa
Rut <R0583231997@email.com>
2026-04-19 23:20:59 +0300

    update api url to render

--- Client/src/service.js
+++ Client/src/service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-
-const apiUrl = "http://localhost:5000"
+console.log("My API URL is:", process.env.REACT_APP_API_URL);
+// לניסיון בלבד - תמחקי את זה אחרי שזה יעבוד
+// const apiUrl = "https://server-yepp.onrender.com";
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
 axios.defaults.baseURL = apiUrl;
 axios.interceptors.response.use(
   response => response, // אם הכל בסדר, פשוט תעביר את התשובה הלאה
